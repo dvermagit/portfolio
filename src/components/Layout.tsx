e42531175b036c9693021cf85eb18616e938e9fe
DHRUVVERMA123 <dhruvmac1234@gmail.com>
2025-06-12 18:58:48 +0530

    layout component

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,4 +1,3 @@
-
 import { useEffect, useState } from "react";
 import { Outlet, useLocation } from "react-router-dom";
 import Navbar from "./Navbar";
@@ -10,12 +9,12 @@ const Layout = () => {
 
   useEffect(() => {
     setIsVisible(false);
-    
+
     const timer = setTimeout(() => {
       setIsVisible(true);
       window.scrollTo(0, 0);
     }, 100);
-    
+
     return () => clearTimeout(timer);
   }, [pathname]);
 
@@ -23,7 +22,11 @@ const Layout = () => {
     <div className="min-h-screen flex flex-col">
       <Navbar />
       <main className="flex-1">
-        <div className={`transition-opacity duration-500 ${isVisible ? 'opacity-100' : 'opacity-0'}`}>
+        <div
+          className={`transition-opacity duration-500 ${
+            isVisible ? "opacity-100" : "opacity-0"
+          }`}
+        >
           <Outlet />
         </div>
       </main>
